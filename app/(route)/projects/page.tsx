2c58c100a0bd2935888ef4c370fa9f9204f554a9
Lee Jungwoo <dlwjdd@naver.com>
2025-11-14 00:27:31 +0900

    feat: POS 만들기 프로젝트 업로드

--- app/(route)/projects/page.tsx
+++ app/(route)/projects/page.tsx
@@ -5,7 +5,7 @@ export default async function Projects() {
   const pages = await Lib.Notion.getPageList(process.env.NOTION_API_PROJECTS_DB_ID!);
 
   return (
-    <div className="w-full grid grid-cols-3 max-w-5xl mx-auto p-6">
+    <div className="w-full grid grid-cols-3 max-w-5xl mx-auto p-6 gap-4">
       {pages.map((db) => {
         // @ts-expect-error db.properties의 타입을 명확하게 정의할 수 없음
         const properties = db.properties;
@@ -14,7 +14,7 @@ export default async function Projects() {
 
         return (
           <Link href={`/projects/${db.id}`} key={db.id}>
-            <Component.Atom.Card className=" border-primary-foreground">
+            <Component.Atom.Card className=" border-primary-foreground h-[150px]">
               <Component.Atom.CardHeader>
                 <Component.Atom.CardTitle>{title}</Component.Atom.CardTitle>
                 <Component.Atom.CardDescription>{desc}</Component.Atom.CardDescription>
